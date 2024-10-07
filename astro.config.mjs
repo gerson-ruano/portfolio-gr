7b57e46725a8040a25f5bbea795091d8c0137661
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-10-07 11:51:06 +0530

    Vite port

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import { SITE_URL } from "./src/data/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
+  server: {
+    host: true,
+    port: 3000
+  },
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
